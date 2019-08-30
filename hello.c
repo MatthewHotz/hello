4f29f448db675475de3c5c2581fc7958f370e475
MatthewHotz <54691705+MatthewHotz@users.noreply.github.com>
2019-08-29 19:24:32 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,14 @@
+/**
+  * Author: Matthew Hotz
+  * Date: 2019/08/29
+  *
+  * A simple hello world program in C
+  *
+  int main(int argc, char **argv) {
+
+printf("Matthew Hotz\n");
+
+printf("MechE\n");
+
+return0;
+}
